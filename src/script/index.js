684900e6a24fbab09672d63a20ef216596536cef
Jiya Gupta <jiya.gupta@joshtechnologygroup.com>
2026-06-25 10:29:37 +0530

    [JG_A1_TESTIMONIALS]: add carousel using splide

--- src/script/index.js
+++ src/script/index.js
@@ -1,3 +1,14 @@
+import Splide from '@splidejs/splide';
+import '@splidejs/splide/css';
+
+ document.addEventListener( 'DOMContentLoaded', function() {
+    var splide = new Splide( '.splide',{
+        type:'loop',
+        // autoplay: true
+    } );
+    splide.mount();
+  } );
+
 const hamburger = document.getElementById('hamburgerBtn');
 const navMenu = document.getElementById('navMenu');
 const closeBtn = document.getElementById("closeBtn");
